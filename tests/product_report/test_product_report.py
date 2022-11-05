ec9c0537d349d8a56e3eb79ef657e62845acc679
Jéssica Florentino <jdgflorentino@gmail.com>
2022-11-05 09:39:07 -0300

    feat: 2 - Testar o relatório individual do produto

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,19 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
+
+mock = "O produto Celular fabricado em 2021-02-08 por Mi Corporation \
+com validade até 2023-09-05 precisa ser armazenado instrucao 15."
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        id=11,
+        nome_do_produto="Celular",
+        nome_da_empresa="Mi Corporation",
+        data_de_fabricacao="2021-02-08",
+        data_de_validade="2023-09-05",
+        numero_de_serie="CR15 1001 4767 6549 4802 1",
+        instrucoes_de_armazenamento="instrucao 15"
+    )
+
+    report = str(product)
+    assert report == mock
