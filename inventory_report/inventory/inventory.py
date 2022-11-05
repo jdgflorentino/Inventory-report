1d6edae430e991ecbf55bdf840b2148486aa4722
Jéssica Florentino <jdgflorentino@gmail.com>
2022-11-05 12:00:26 -0300

    feat: 6 - Gere os relatórios através de um arquivo JSON

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,6 +1,7 @@
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 import csv
+import json
 
 
 class Inventory():
@@ -8,6 +9,10 @@ class Inventory():
         if path.endswith(".csv"):
             with open(path) as file:
                 return list(csv.DictReader(file, delimiter=",", quotechar='"'))
+
+        if path.endswith(".json"):
+            with open(path) as file:
+                return list(json.load(file))
 
     @staticmethod
     def import_data(path, type):
